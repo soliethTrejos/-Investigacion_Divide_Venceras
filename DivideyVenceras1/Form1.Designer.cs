2f8c52c32e79ba3691e42ed08525d5dd4b2dabab
Daniel Gutiérrez <d.xander.gutierrez@gmail.com>
2024-10-10 21:25:46 -0600

    Max/Min

---
+++ DivideyVenceras1/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace DivideyVenceras1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbNum = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.lblMax = new System.Windows.Forms.Label();
+            this.lblMin = new System.Windows.Forms.Label();
+            this.tbNum = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingresar número:";
+            // 
+            // lbNum
+            // 
+            this.lbNum.FormattingEnabled = true;
+            this.lbNum.ItemHeight = 16;
+            this.lbNum.Location = new System.Drawing.Point(16, 72);
+            this.lbNum.Name = "lbNum";
+            this.lbNum.Size = new System.Drawing.Size(120, 132);
+            this.lbNum.TabIndex = 1;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(123, 35);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 24);
+            this.btnAdd.TabIndex = 2;
+            this.btnAdd.Text = "Agregar";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(29, 210);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(91, 23);
+            this.btnCalculate.TabIndex = 3;
+            this.btnCalculate.Text = "Max/Min";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // lblMax
+            // 
+            this.lblMax.AutoSize = true;
+            this.lblMax.Location = new System.Drawing.Point(16, 241);
+            this.lblMax.Name = "lblMax";
+            this.lblMax.Size = new System.Drawing.Size(57, 16);
+            this.lblMax.TabIndex = 4;
+            this.lblMax.Text = "Máximo:";
+            // 
+            // lblMin
+            // 
+            this.lblMin.AutoSize = true;
+            this.lblMin.Location = new System.Drawing.Point(19, 276);
+            this.lblMin.Name = "lblMin";
+            this.lblMin.Size = new System.Drawing.Size(53, 16);
+            this.lblMin.TabIndex = 5;
+            this.lblMin.Text = "Mínimo:";
+            // 
+            // tbNum
+            // 
+            this.tbNum.Location = new System.Drawing.Point(23, 35);
+            this.tbNum.Name = "tbNum";
+            this.tbNum.Size = new System.Drawing.Size(81, 22);
+            this.tbNum.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tbNum);
+            this.Controls.Add(this.lblMin);
+            this.Controls.Add(this.lblMax);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lbNum);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox lbNum;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label lblMax;
+        private System.Windows.Forms.Label lblMin;
+        private System.Windows.Forms.TextBox tbNum;
+    }
+}
+
---
+++ DivideyVenceras1/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DivideyVenceras1
+{
+    public partial class Form1 : Form
+    {
+        // Definir arreglo para 8 elementos
+        private int[] numeros = new int[8];
+        private int i = 0; 
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int numero = int.Parse(tbNum.Text);
+
+                if (i < numeros.Length)
+                {
+                    numeros[i] = numero;
+                    i++;
+                    lbNum.Items.Add(numero);
+                    tbNum.Clear();
+                }
+                else
+                {
+                    MessageBox.Show("Ha alcanzado el límite de elementos.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Por favor, ingresa un número válido.");
+            }
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (i > 0)
+            {
+                int[] numerosIngresados = new int[i];
+                Array.Copy(numeros, numerosIngresados, i);
+
+                int maxVal = CalcularMaximo(numerosIngresados);
+                int minVal = CalcularMinimo(numerosIngresados);
+
+                // Mostrar resultados
+                lblMax.Text = $"Máximo: {maxVal}";
+                lblMin.Text = $"Mínimo: {minVal}";
+            }
+            else
+            {
+                MessageBox.Show("Por favor, agrega números antes de calcular.");
+            }
+        }
+
+        private int CalcularMaximo(int[] arr)
+        {
+            return max(arr, 0, arr.Length - 1);
+        }
+
+        private int CalcularMinimo(int[] arr)
+        {
+            return min(arr, 0, arr.Length - 1);
+        }
+
+        // Divide y vencerás
+        private int max(int[] arr, int inicio, int fin)
+        {
+            if (inicio == fin)
+                return arr[inicio];
+
+            int medio = (inicio + fin) / 2;
+
+            int maxIzq = max(arr, inicio, medio);
+            int maxDer = max(arr, medio + 1, fin);
+
+            return Math.Max(maxIzq, maxDer);
+        }
+
+        // Método recursivo de Divide y Vencerás para encontrar el mínimo
+        private int min(int[] arr, int inicio, int fin)
+        {
+            if (inicio == fin)
+                return arr[inicio];
+
+            int medio = (inicio + fin) / 2;
+
+            int minIzq = min(arr, inicio, medio);
+            int minDer = min(arr, medio + 1, fin);
+
+            return Math.Min(minIzq, minDer);
+        }
+    }
+}
