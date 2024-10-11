1a74cc20ca3b23bdbedd491cb64a5b32442286e7
3DG4RDC26 <ercasco@uamv.edu.ni>
2024-10-10 22:34:50 -0600

    Estructura de Paradigma

--- Suma en un Arreglo/Form1.Designer.cs
+++ Suma en un Arreglo/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.btnCalcular = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.lblResult = new System.Windows.Forms.Label();
@@ -81,6 +82,7 @@
             this.Controls.Add(this.lblResult);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.btnCalcular);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Suma de numeros";
             this.ResumeLayout(false);
